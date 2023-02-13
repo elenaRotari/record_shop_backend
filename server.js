3aff1ef24e23aeb6adb61e5cd2eecafd58a61ba9
Elena Rotari <elena.rotari@icloud.com>
2023-02-13 14:37:03 +0100

    express.static(dist added

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
   })
 );
 
@@ -27,6 +27,7 @@ app.use("/api/products", productsRouter);
 app.use("/api/users", userRoute);
 
 app.use(express.static("uploads"));
+app.use(express.static("dist"));
 app.use("/:notfound", notFound);
 
 app.use(error);
